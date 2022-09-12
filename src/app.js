be457da3171498aabf8c7f1595abdee15fe580fd
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-09-12 11:57:17 +0600

    feat: add body-parser

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   /* eslint-disable-next-line no-console */
