9b4719c85cbe8156c11f674fd494406458b3f426
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-09-20 20:21:10 +0600

    feat: modify user model, plug-in validator

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: (props) => `${props.value} не является валидным email`,
+    },
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
   name: {
     type: String,
     required: true,
