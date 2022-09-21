c538606d2477d447122d8d67a7750d7f41189fb3
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-09-21 16:18:36 +0600

    fix: remove hardcode authorization

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const useMainRouter = require('./routes');
-// const authorization = require('./utils/authorization');
 const errorHandler = require('./middlewares/errorHandler');
 const notFoundHandler = require('./middlewares/notFoundHandler');
 
@@ -15,15 +14,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(authorization);
-app.use((req, res, next) => {
-  req.user = {
-    _id: '631efb2d9a828dce250c0779',
-  };
-
-  next();
-});
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
