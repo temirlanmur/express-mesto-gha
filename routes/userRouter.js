7067bc7011ca152e65a0ab90ca1cdd3712873748
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-09-21 22:36:35 +0600

    fix: restructure user routing

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,11 +8,12 @@ const {
   updateProfileAvatar,
 } = require('../controllers/userController');
 
-userRouter.get('/', getUsers);
-userRouter.get('/:userId', getUser);
-userRouter.post('/', createUser);
 userRouter.get('/me', getProfile);
 userRouter.patch('/me', updateProfile);
 userRouter.patch('/me/avatar', updateProfileAvatar);
+
+userRouter.get('/', getUsers);
+userRouter.get('/:userId', getUser);
+userRouter.post('/', createUser);
 
 module.exports = userRouter;
