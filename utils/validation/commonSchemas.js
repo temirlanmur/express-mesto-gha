37071db5c96008c646374f2ce773ddaf88432eda
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-09-23 16:13:38 +0600

    refactor: use custom pattern for url validation

--- utils/validation/commonSchemas.js
+++ utils/validation/commonSchemas.js
@@ -2,7 +2,7 @@ const { Joi } = require('celebrate');
 
 const commonSchemas = {
   id: Joi.string().alphanum().length(24),
-  url: Joi.string().uri(),
+  url: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=]*)/),
 };
 
 module.exports = commonSchemas;
